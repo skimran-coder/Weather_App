5eae8012a3ee4c4bb40e34de5b722d6cada9c316
Sk. Imran Hussain <skimranpc@gmail.com>
2023-10-06 15:18:11 +0530

    corrected event listener on click and api call

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const tabs = document.querySelectorAll('.tab')
 const yourWeather = document.querySelector('[userWeather]');
 const searchWeather = document.querySelector('[searchWeather]');
-const loadingScreen = document.querySelector('.loading-container')
 const grantAccessScreen = document.querySelector('.grant-access-container')
 const accessButton = document.querySelector('.grant-access-btn')
+const loadingScreen = document.querySelector('.loading-container')
+const searchScreen = document.querySelector('.search-weather-container')
+const weatherScreen = document.querySelector('.weather-container')
+
 
 const API_key = 'd7f8e1a8f9bf07da8fd33aa4650f59b2';
+
+let storedLocation;
 let currentTab = yourWeather
 
 
@@ -26,6 +31,7 @@ function changeTab(e) {
 
     // when on clicked on your weather tab
     if (e.target === yourWeather) {
+        searchScreen.style.display = 'none';
         displayLoadingScreen()
         checkAccess()
     }
@@ -36,6 +42,14 @@ function changeTab(e) {
     }
 }
 
+// display search bar
+function displaySearchbar(){
+    grantAccessScreen.style.display = 'none';
+    weatherScreen.style.display = 'none';
+    searchScreen.style.display = 'flex';
+}
+
+
 // displaying loading screen
 function displayLoadingScreen(){
     loadingScreen.style.display = 'flex';
@@ -45,7 +59,7 @@ function displayLoadingScreen(){
 function checkAccess(){
 
     // checking if co-ordinates are already in local storage
-    if (storedLocation != "") {
+    if (false) {
         console.log("will write condition if access already given");
     }
 
@@ -61,7 +75,7 @@ function displayAccessScreen(){
     grantAccessScreen.style.display = 'flex';
 }
 
-accessButton.addEventListener('clcik', grantAccess)
+accessButton.addEventListener('click', grantAccess)
 
 // getting location when clicked on button
 function grantAccess(){
@@ -71,23 +85,26 @@ function grantAccess(){
     else {
         alert("Geolocation is not supported in your device");
     }
+
+    // getting co-ordiantes -- will save in local storage
+    function storeLocation(position){
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude
+        
+        console.log(`${latitude} & ${longitude}`);
+        // have to save co-ords in local storage??
+    
+        // calling function to find weather by sending lat/long
+        findWeather(latitude, longitude)
+    }
 }
 
-// getting co-ordiantes -- will save in local storage
-function storeLocation(){
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude
 
-    // have to save co-ords in local storage??
 
-    // calling function to find weather by sending lat/long
-    findWeather(latitude, longitude)
-}
 
 // api call to get response
 async function findWeather(latitude, longitude){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_key}`)
-    console.log(response.json());
+    const result = await response.json()
+    console.log(result);
 }
-
-findWeather(19.075983, 72.877655)
